0894d0ef3f2f3cc09d1888fd5c53923305fc88d3
TrshPuppy <trshpuppy@proton.me>
2023-10-24 10:28:07 -0400

    organize CustomPortAction + other cleanup

--- netpuppy/__main__.py
+++ netpuppy/__main__.py
@@ -22,41 +22,7 @@ def main() -> None:
     # Get the Args from command line:
     cli_args = get_parse_args()
     print(f"args in main: {cli_args}")
-    # Make the CLI arg parser:
-    # parser = argparse.ArgumentParser(
-    #     prog="netpuppy",  # ./netpuppy
-    #     description="Launch a puppy to sneef and fetch data for you",
-    #     epilog="Tell netpuppy he was a good boi.",
-    # )
 
-    # # Add arguments
-    # #   First group is mutually exclusive (listen vs host-ip/ connect)
-    # exclusive_group = parser.add_mutually_exclusive_group(required=True)
-    # exclusive_group.add_argument("-l", "--listen", action="store_true")
-    # exclusive_group.add_argument("-H", "--host-ip", action="store")
-    # # HOST NOTES:
-    # #   Limit to real IP addresses
-    # #   Return and error if the IP is not valid
-    # #   be able to take both IP addresses and/ or hostnames
-    # #       IP vs Hostname
-    # #           argparse probs cant check the input
-    # #           INSTEAD:
-    # #              regex the input for IP format (x.x.x.x) OR (x:x:x:x:x:x:x:x)
-    # #                   handle ipv4 and ipv6 formats (multiple)
-    # #                   handle ipv4 embedded into ip4 6
-    # #   `                ETC`
-    # #              + (if the input fails that regex check, treat it as
-    # #                   a possible domain/ hostname instead)
-
-    # parser.add_argument(
-    #     "-p", "--port", action="store", required=True, type=int, nargs=1
-    # )
-    # # PORT NOTES:
-    # # Limit port range
-
-    # # Get the list of arguments:
-    # args = parser.parse_args()
-    # print(args)
     return 0
 
 
@@ -67,18 +33,29 @@ def get_parse_args():
         epilog="Tell netpuppy he was a good boi.",
     )
 
+    # This class handles input for the --port flag which has multiple valid formats
     class CustomPortAction(argparse.Action):
+        print(f"custom action outside call")
+        # PORT NOTES:
+        #   Limit individual port range
+        #   Allow for a list of ports
+        #       should be: range, or list (ie. 1-10 vs 1,5,7)
+
+        #    parser = ArgumentParser(prog='netpuppy', usage=None, description='Launch a puppy to sneef and fetch data for you', formatter_class=<class 'argparse.HelpFormatter'>, conflict_handler='error', add_help=True): namespace = Namespace(listen=True, host_ip=None, port=None): values= ['20-55']: option_string = -p
+        #    Namespace(listen=True, host_ip=None, port=None)
+
+        # ArgParser enters here:
         def __call__(self, parser, namespace, values, option_string=None):
             print(f"custom actiont")
-            # print(
-            #     f"parser = {parser}: namespace = {namespace}: values= {values}: option_string = {option_string}"
-            # )
-            #    parser = ArgumentParser(prog='netpuppy', usage=None, description='Launch a puppy to sneef and fetch data for you', formatter_class=<class 'argparse.HelpFormatter'>, conflict_handler='error', add_help=True): namespace = Namespace(listen=True, host_ip=None, port=None): values= ['20-55']: option_string = -p
-            #    Namespace(listen=True, host_ip=None, port=None)
 
+            # Set self.values to values for --port flag stored from argparse
             setattr(self, "values", values)
-            # port_input = parser["get_values"]
-            print(f"self = {self.values}")
+
+            # ULTIMATEL GOAL OF THIS CLASS:
+            #   Set values on namespace object to correct values
+            setattr(namespace, "port", "tiddies")
+
+            return 0
 
     # Add arguments
     #   First group is mutually exclusive (listen vs host-ip/ connect)
@@ -94,8 +71,7 @@ def get_parse_args():
     #           INSTEAD:
     #              regex the input for IP format (x.x.x.x) OR (x:x:x:x:x:x:x:x)
     #                   handle ipv4 and ipv6 formats (multiple)
-    #                   handle ipv4 embedded into ip4 6
-    #   `                ETC`
+    #                   handle ipv4 embedded into ip4 6 ETC
     #              + (if the input fails that regex check, treat it as
     #                   a possible domain/ hostname instead)
 
@@ -103,10 +79,6 @@ def get_parse_args():
     parser.add_argument(
         "-p", "--port", action=CustomPortAction, required=True, nargs="+"
     )
-    # PORT NOTES:
-    #   Limit individual port range
-    #   Allow for a list of ports
-    #       should be: range, or list (ie. 1-10 vs 1,5,7)
 
     parser.add_argument("-v", "--version", action="version", version="NetPuppy v1.0")
 
