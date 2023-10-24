e3dc9d4260b2ddb3e990fc824c5b57543d91dd33
TrshPuppy <trshpuppy@proton.me>
2023-10-24 09:34:57 -0400

    Add some notes.

--- netpuppy/__main__.py
+++ netpuppy/__main__.py
@@ -1,11 +1,21 @@
 ##################### TP U ARE HERE
-####################### get rid of nargs?
-###################### group. add_argument: does it take the same parameters
-###################### as regular parser.add_argument?
-##################### check out cool-retro-term
+# Get a hostname, throw it to a IP-lookup library.
+# A good library should just return the IP-address,
+# if you give it an IP-address instead of a hostname.
+# (Note: One hostname can have multiple IPv4 and/or IPv6 addresses,
+#  so prepare to try all of them until you can make a connection.)
+# find library to help with domain name resolution
+#   OTHER:
+#       mypy complains about types (check this/ K.I.M.)
+#       unit tests for argparse
+#       dev branch
+#           protect pushed to main (need to validatae)\
+#           CICD w/ github actions
+#           formatting tests
 
 import argparse
 import socket
+import ipaddress
 
 
 def main() -> None:
@@ -21,8 +31,25 @@ def main() -> None:
     exclusive_group = parser.add_mutually_exclusive_group(required=True)
     exclusive_group.add_argument("-l", "--listen", action="store_true")
     exclusive_group.add_argument("-H", "--host-ip", action="store")
+    # HOST NOTES:
+    #   Limit to real IP addresses
+    #   Return and error if the IP is not valid
+    #   be able to take both IP addresses and/ or hostnames
+    #       IP vs Hostname
+    #           argparse probs cant check the input
+    #           INSTEAD:
+    #              regex the input for IP format (x.x.x.x) OR (x:x:x:x:x:x:x:x)
+    #                   handle ipv4 and ipv6 formats (multiple)
+    #                   handle ipv4 embedded into ip4 6
+    #   `                ETC`
+    #              + (if the input fails that regex check, treat it as
+    #                   a possible domain/ hostname instead)
 
-    parser.add_argument("-p", "--port", action="store", nargs=1, required=True)
+    parser.add_argument(
+        "-p", "--port", action="store", required=True, type=int, nargs=1
+    )
+    # PORT NOTES:
+    # Limit port range
 
     # Get the list of arguments:
     args = parser.parse_args()
