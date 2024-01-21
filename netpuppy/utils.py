e64e53d5f6415d48033c7dc123fb8f4d75f06e6e
bepursuant <kklika@pursuantsolutions.com>
2024-01-21 01:58:49 -0600

    Fix bad escape sequences that were causing `make test` to throw warnings

--- netpuppy/utils.py
+++ netpuppy/utils.py
@@ -8,8 +8,8 @@ TrshPuppy brings you...
 
 |8PPPPe                  ___      .++.
 |8    |8 |eeee |eeeee __/_, `.  .'    `. .
-|8e   |8 |8      |8   \_,  | \_'  /   )`-')
-|88   |8 |8eee   |8e   U ) `-`    \  ((`\"`
+|8e   |8 |8      |8   \\_,  | \\_'  /   )`-')
+|88   |8 |8eee   |8e   U ) `-`    \\  ((`\"`
 |88   |8 |88     |88   ___Y  ,    .'7 /| 
 |88___|8_|88ee___|88__(_,___/___.'_(_/_/_
 
@@ -47,7 +47,7 @@ def user_selection_update(h: str, p: str, c: str) -> str:
         update = """
     .-.  *sneef sneef*
    / (_   
-  ( "  6\___o   |Host: {host}
+  ( "  6\\___o   |Host: {host}
   /  (  ___/    |Port: {port}
  /     /  U     |Mode: {mode}
     """.format(
