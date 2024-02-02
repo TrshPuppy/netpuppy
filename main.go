8325c7d38dbd2eb6dd18b82f90050c2cecb305dc
TrshPuppy <trshpuppy@proton.me>
2024-02-02 15:23:23 -0500

    the args are pased.

--- main.go
+++ main.go
@@ -6,29 +6,59 @@ import (
 )
 
 func main() {
-	fmt.Println("Tiddies makde it to the chat!")	
+	fmt.Println("Tiddies makde it to the chat!")
 
 	// Set flag values based on input:
 	listenFlag := flag.Bool("l", false, "put NetPuppy in listen mode")
-	hostFlag := flag.String("H", "127.0.0.1", "target host IP address to connect to")
-	portFlag := flag.Int("p", 44444, "target port")
+	hostFlag := flag.String("H", "0.0.0.0", "target host IP address to connect to")
+	portFlag := flag.Int("p", 40404, "target port")
 
 	// Parse command line arguments:
 	flag.Parse()
-	fmt.Printf("The listen flag is %v\n", *listenFlag)
-	fmt.Printf("The host flag is %v\n", *hostFlag)
-	fmt.Printf("The port is %v\n", *portFlag)
+
+	// Depending on input, create this peer's type:
+	type peer struct {
+		connection_type string
+		port            int
+		address         string
+		hostname        string
+	}
+
+	// Initiate peer struct
+	thisPeer := peer{port: *portFlag, address: *hostFlag}
+
+	// If -l was given, create an offense peer:
+	if *listenFlag {
+		thisPeer.connection_type = "offense"
+		thisPeer.address = "0.0.0.0"
+	} else {
+		thisPeer.connection_type = "connect_back"
+	}
+
+	fmt.Printf("The connection type is: %s\n", thisPeer.connection_type)
+	fmt.Printf("The host is %s\n", thisPeer.address)
+	fmt.Printf("The port is %v\n", thisPeer.port)
 
 	/*
-		if -l is on, 
+		if -l is on,
+			net.Listen('tcp', PORT)
 			set connection address for socket to any
 		if not
 			connection address = host flag
 
+
+		struct/ objsect thing (this peer)
+			- connect back (executed on the target)
+				- start the subprocess
+			- offense (exe on hacker machine)
+				- keeep taking user input
+
+
+
+
+
 	*/
 
 	// Try to create connection:
-	if listenFlag* {
-		
-	}
+
 }
