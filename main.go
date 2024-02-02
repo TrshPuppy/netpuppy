a3abc8d090b768d68977de30a479d4619a8a766e
TrshPuppy <trshpuppy@proton.me>
2024-02-02 18:04:53 -0500

    The connection has been implemented.

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"flag"
 	"fmt"
+	"net"
+	"os"
 )
 
 func main() {
@@ -11,9 +13,10 @@ func main() {
 	// Set flag values based on input:
 	listenFlag := flag.Bool("l", false, "put NetPuppy in listen mode")
 	hostFlag := flag.String("H", "0.0.0.0", "target host IP address to connect to")
-	portFlag := flag.Int("p", 40404, "target port")
+	turdnuggies := flag.Int("p", 40404, "target port") // portFlag @Trauma_x_Sella
 
 	// Parse command line arguments:
+	//                                            error?
 	flag.Parse()
 
 	// Depending on input, create this peer's type:
@@ -21,11 +24,11 @@ func main() {
 		connection_type string
 		port            int
 		address         string
-		hostname        string
+		connection      net.Conn
 	}
 
 	// Initiate peer struct
-	thisPeer := peer{port: *portFlag, address: *hostFlag}
+	thisPeer := peer{port: *turdnuggies, address: *hostFlag}
 
 	// If -l was given, create an offense peer:
 	if *listenFlag {
@@ -38,6 +41,38 @@ func main() {
 	fmt.Printf("The connection type is: %s\n", thisPeer.connection_type)
 	fmt.Printf("The host is %s\n", thisPeer.address)
 	fmt.Printf("The port is %v\n", thisPeer.port)
+
+	// Now that we have our peer: try to make connection
+	var asyncio_rocks net.Conn // connection @0xtib3rius
+	var err error
+
+	if thisPeer.connection_type == "offense" {
+		listener, err1 := net.Listen("tcp", fmt.Sprintf(":%v", thisPeer.port))
+		if err1 != nil {
+			fmt.Printf("Error when creating listener: %v\n", err1)
+			os.Stderr.WriteString(err1.Error())
+			os.Exit(1)
+		}
+
+		asyncio_rocks, err = listener.Accept()
+		if err != nil {
+			os.Stderr.WriteString(err.Error())
+			os.Exit(1)
+			//  log.Fatal(err1.Error()
+		}
+
+	} else {
+		// remoteHost := [2]string{thisPeer.address, thisPeer.port}
+		remoteHost := fmt.Sprintf("%v:%v", thisPeer.address, thisPeer.port)
+		asyncio_rocks, err = net.Dial("tcp", remoteHost)
+		if err != nil {
+			os.Stderr.WriteString(err.Error())
+			os.Exit(1)
+		}
+	}
+
+	// Now that we have a connection, read it/ write to it
+	fmt.Printf("the connection is : %v", asyncio_rocks)
 
 	/*
 		if -l is on,
@@ -54,11 +89,16 @@ func main() {
 				- keeep taking user input
 
 
-
-
-
+			- method:
+				func make connection(){
+					if this.type = offense:
+						connection = net.Listener
+						(needs Accept() to actually become a connection)
+					else:
+						connection = net.Dial
+				}
 	*/
 
 	// Try to create connection:
-
+	return
 }
