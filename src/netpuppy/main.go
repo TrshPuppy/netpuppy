00fc2758943f6855547d1d7ca87ba6f7a6a068db
TrshPuppy <trshpuppy@proton.me>
2024-02-07 18:39:40 -0500

    handle stdin.

--- src/netpuppy/main.go
+++ src/netpuppy/main.go
@@ -37,6 +37,8 @@ func readFromSocket(socketReader chan<- []byte, connection net.Conn) {
 		dataBytes, err := bufio.NewReader(connection).ReadBytes('\n')
 		if err != nil {
 			fmt.Printf("Error reading from socket: %v\n", err)
+			os.Stderr.WriteString(err.Error())
+			os.Exit(1)
 			return
 		}
 		socketReader <- dataBytes
@@ -47,6 +49,7 @@ func startHelperShell() (*exec.Cmd, error) { // @Trauma_X_Sella 'connection'
 	bashPath, err := exec.LookPath(`/bin/bash`)
 	if err != nil {
 		fmt.Printf("Error finding bash path: %v\n", err)
+		os.Stderr.WriteString(" " + err.Error() + "\n")
 		return nil, err
 	}
 
@@ -67,6 +70,11 @@ func main() {
 
 	// Print banner:
 	fmt.Printf("%s", utils.Banner())
+
+	// Get STDIN and save to a variable we can use if we need:
+	stdinReader := bufio.NewReader(os.Stdin)
+	stdin, _ := stdinReader.ReadString('\n')
+	fmt.Printf("STDIN = %v", stdin) // Keep for now to avoid golang complaints about unused vars.
 
 	// Depending on input, create this peer's type:
 	type peer struct {
@@ -97,7 +105,7 @@ func main() {
 		listener, err1 := net.Listen("tcp", fmt.Sprintf(":%v", thisPeer.rPort))
 		if err1 != nil {
 			fmt.Printf("Error when creating listener: %v\n", err1)
-			os.Stderr.WriteString(err1.Error())
+			os.Stderr.WriteString(" " + err.Error() + "\n")
 			os.Exit(1)
 		}
 
@@ -105,7 +113,7 @@ func main() {
 
 		asyncio_rocks, err = listener.Accept()
 		if err != nil {
-			os.Stderr.WriteString(err.Error())
+			os.Stderr.WriteString(" " + err.Error() + "\n")
 			os.Exit(1)
 			//  log.Fatal(err1.Error()
 		}
@@ -119,7 +127,7 @@ func main() {
 			asyncio_rocks, err = net.Dial("tcp", remoteHost)
 
 			if err != nil {
-				os.Stderr.WriteString(err.Error())
+				os.Stderr.WriteString(" " + err.Error() + "\n")
 				os.Exit(1)
 			}
 		}
@@ -155,6 +163,7 @@ func main() {
 		if shellStartErr != nil {
 			fmt.Printf("Error starting shell process: %v\n", shellStartErr)
 			thisPeer.connection.Close()
+			os.Stderr.WriteString(" " + shellStartErr.Error() + "\n")
 			os.Exit(1)
 		}
 		thisPeer.cbShell = connectBackShell
@@ -176,12 +185,14 @@ func main() {
 			if err != nil {
 				// Quit here?
 				fmt.Printf("Error in userInput select: %v\n", err)
+				os.Stderr.WriteString(" " + err.Error() + "\n")
 			}
 		case socketIncoming := <-socketReader:
 			_, err := os.Stdout.Write(socketIncoming)
 			if err != nil {
 				// Quit here?
 				fmt.Printf("Error in writing to stdout: %v\n", err)
+				os.Stderr.WriteString(" " + err.Error() + "\n")
 			}
 		default:
 			time.Sleep(300 * time.Millisecond)
