db6daed907558a81ddbfd411a22f46429d769dd9
rawoke083 <jacquesdr@gmail.com>
2024-06-18 23:25:45 +0200

    Replace custom pty implementation with github.com/creack/pty package, easier to build and cross-compile

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/trshpuppy/netpuppy
 go 1.22.1
 
 //require github.com/trshpuppy/pty v1.0.0
+
+require github.com/creack/pty v1.1.21
--- pkg/pty/pty.go
+++ pkg/pty/pty.go
@@ -1,47 +1,22 @@
 package pty
 
 import (
+	"fmt"
 	"os"
+
+	"github.com/creack/pty"
 )
 
 func GetPseudoterminalDevices() (*os.File, *os.File, error) {
-	mDevice, sDevice, err := Start()
-
-	return mDevice, sDevice, err
+	return Start()
 }
 
 func Start() (*os.File, *os.File, error) {
-	var mptr *os.File
-	var sname string
-	var err error
-
-	// Get pseudoterminal master from /dev/ptmx
-	mptr, err = os.OpenFile("/dev/ptmx", os.O_RDWR, os.ModeDevice)
+	// Open a new pty
+	mptr, sptr, err := pty.Open()
 	if err != nil {
-		return nil, nil, err
+		return nil, nil, fmt.Errorf("failed to open pty: %w", err)
 	}
 
-	// Get the name of the slave device:
-	sname, err = GetPTSName(mptr)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	// Now that we have the name, we have to call grantpt() & unlockpt():
-	err = GrantPT(mptr)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	err = UnlockPt(mptr)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	// Now that permission is granted, and the slave is unlocked, we can open the pts device file:
-	sptr, err := os.OpenFile(sname, os.O_RDWR, os.ModeDevice)
-	if err != nil {
-		return nil, nil, err
-	}
 	return mptr, sptr, nil
 }
